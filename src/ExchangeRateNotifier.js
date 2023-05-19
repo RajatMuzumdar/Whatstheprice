4ffe9ed3f2733f2f2537327eb11d08249c8e78d4
RajatMuzumdar <104819793+RajatMuzumdar@users.noreply.github.com>
2023-05-19 23:05:51 +0530

    Update ExchangeRateNotifier.js
    typography giving error checking

--- src/ExchangeRateNotifier.js
+++ src/ExchangeRateNotifier.js
@@ -64,7 +64,7 @@ const ExchangeRateNotifier = () => {
         margin: "5ch",
         boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
       }}
-    > <Typography variant="h6">
+    > <Typography >
       Get Exchange Rate Notifications EveryDay
     </Typography>
       <form onSubmit={handleSubmit}>
