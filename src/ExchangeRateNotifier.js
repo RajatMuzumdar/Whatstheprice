d42cc33a29c8fcb6c22f5280445003c432ac3705
RajatMuzumdar <104819793+RajatMuzumdar@users.noreply.github.com>
2023-05-19 22:33:43 +0530

    Update ExchangeRateNotifier.js typography

--- src/ExchangeRateNotifier.js
+++ src/ExchangeRateNotifier.js
@@ -63,6 +63,9 @@ const ExchangeRateNotifier = () => {
         boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
       }}
     >
+         <Typography variant="h6">
+      Get Exchange Rate Notifications EveryDay
+    </Typography>
       <form onSubmit={handleSubmit}>
         <TextField
           sx={{ m: 1, minWidth: "37ch" }}
